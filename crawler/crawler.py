bef0c9b642c6d75245e0f4c480c45bce097dc138
Burak Yilmaz <burakyilmaz321@gmail.com>
2020-07-26 12:46:35 +0300

    Linting

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,18 +1,36 @@
+"""
+This script crawls data from Tefas and saves it to S3.
+
+Usage
+
+$ python crawler.py --start-date STARTDATE --end-date ENDDATE
+
+or
+
+$ python crawler.py --start-date DATE
+
+"""
+# pylint: disable=missing-function-docstring
+
 import argparse
-from datetime import datetime
+import json
 import logging
 import os
-import json
 import sys
+from datetime import datetime
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 def get_logger():
+    """
+    Return a logger for with console and file handler.
+    Create log folder if not exists.
+    """
     log_dir = os.path.join(os.path.dirname(__file__), "log")
     latest_log = 0
     try:
@@ -25,15 +43,14 @@ def get_logger():
     log_file = os.path.join(log_dir, f"{str(latest_log + 1)}.log")
     log = logging.getLogger(__name__)
     log.setLevel(logging.INFO)
-    fh = logging.FileHandler(log_file)
-    fh.setLevel(logging.INFO)
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)
-    formatter = logging.Formatter(
-        "[%(asctime)s][%(name)s][%(levelname)s]: %(message)s")
-    fh.setFormatter(formatter)
-    log.addHandler(fh)
-    log.addHandler(ch)
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(logging.INFO)
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    formatter = logging.Formatter("[%(asctime)s][%(name)s][%(levelname)s]: %(message)s")
+    file_handler.setFormatter(formatter)
+    log.addHandler(file_handler)
+    log.addHandler(console_handler)
     return log
 
 
@@ -52,20 +69,22 @@ FIRST_ROW_XPATH = '//*[@id="MainContent_GridViewGenel"]/tbody/tr[2]/td[1]'
 
 
 def parse_table(content):
+    """Parse an html tbody object. Return every row as a dict."""
     soup = BeautifulSoup(content, features="html.parser")
     data = []
-    table = soup.find('tbody')
-    rows = table.find_all('tr')
-    header = rows.pop(0).find_all('th')
+    table = soup.find("tbody")
+    rows = table.find_all("tr")
+    header = rows.pop(0).find_all("th")
     header = [ele.text.strip() for ele in header]
     for row in rows:
-        cols = row.find_all('td')
+        cols = row.find_all("td")
         cols = [ele.text.strip() for ele in cols]
         data.append(dict(zip(header, cols)))
     return data
 
 
 def parse_pages(driver):
+    """Iterate over pages and parse content."""
     LOG.info("Start")
     wait = WebDriverWait(driver, 60)
     pages = []
@@ -82,7 +101,7 @@ def parse_pages(driver):
         if next_button:
             # selenium WebDriver click() does not work here for some reason
             driver.execute_script(JQUERY_CLICK(NEXT_BUTTON_ID))
-            # wait for the next page load
+            # wait until the next page is loaded
             wait.until(
                 EC.text_to_be_present_in_element(
                     (By.ID, PAGE_NUM_ID), str(len(pages) + 1)
@@ -96,9 +115,7 @@ def parse_pages(driver):
 
 def main():
     # parse command line arguments
-    parser = argparse.ArgumentParser(
-        description="Crawls price data"
-    )
+    parser = argparse.ArgumentParser(description="Crawls price data")
     parser.add_argument(
         "--start-date",
         help="first date to crawl. format should be DD.MM.YYYY",
@@ -127,7 +144,7 @@ def main():
     # start crawling
     LOG.info(f"Crawling from {start_date} to {end_date}")
     LOG.info("Connecting to the driver...")
-    s = datetime.now()
+    start = datetime.now()
     with webdriver.Safari() as driver:
         driver.get(ROOT_URL)
         driver.find_element_by_id(START_DATE_ID).send_keys(start_date)
@@ -135,18 +152,15 @@ def main():
         # selenium WebDriver click() does not work here for some reason
         driver.execute_script(JQUERY_CLICK(SEARCH_BUTTON_ID))
         WebDriverWait(driver, 60).until(
-            EC.text_to_be_present_in_element(
-                (By.XPATH, FIRST_ROW_XPATH),
-                end_date
-            )
+            EC.text_to_be_present_in_element((By.XPATH, FIRST_ROW_XPATH), end_date)
         )
         pages = parse_pages(driver)
-    e = datetime.now()
-    LOG.info(f"Crawling completed in {(e - s).total_seconds()} secs")
+    end = datetime.now()
+    LOG.info(f"Crawling completed in {(end - start).total_seconds()} secs")
 
     with open("data.json", "w", encoding="utf-8") as outf:
         json.dump(pages, outf, indent=2, ensure_ascii=False)
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     sys.exit(main())
